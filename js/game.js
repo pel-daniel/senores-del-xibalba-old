8326639194a86bc20819d3d90990b326e7fee43f
Daniel Garcia <pel_daniel@hotmail.com>
2016-01-31 00:13:25 -0600

    Players move with the keyboard

--- js/game.js
+++ js/game.js
@@ -8,6 +8,9 @@ var properties = {
     width: 800,
     height: 400,
   },
+  player: {
+    velocity: 400,
+  },
   leftPlayer: {
     x: 50,
   },
@@ -41,13 +44,31 @@ mainState.prototype = {
 
   create: function () {
     this.initGraphics();
+    this.initKeyboard();
     this.initPhysics();
 
     game.input.onDown.add(this.startGame, this);
   },
 
   update: function () {
+    this.updatePlayer(
+      this.leftPlayerSprite,
+      this.leftPlayerUpKey,
+      this.leftPlayerDownKey
+    );
+    this.updatePlayer(
+      this.rightPlayerSprite,
+      this.rightPlayerUpKey,
+      this.rightPlayerDownKey
+    );
+  },
 
+  initKeyboard: function() {
+    this.leftPlayerUpKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    this.leftPlayerDownKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
+
+    this.rightPlayerUpKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    this.rightPlayerDownKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
   },
 
   initGraphics: function () {
@@ -84,6 +105,28 @@ mainState.prototype = {
     this.ballSprite.body.collideWorldBounds = true;
     this.ballSprite.body.immovable = true;
     this.ballSprite.body.bounce.set(1);
+
+    this.playersGroup = game.add.group();
+    this.playersGroup.enableBody = true;
+    this.playersGroup.physicsBodyType = Phaser.Physics.ARCADE;
+
+    this.playersGroup.add(this.leftPlayerSprite);
+    this.playersGroup.add(this.rightPlayerSprite);
+
+    this.playersGroup.setAll('checkWorldBounds', true);
+    this.playersGroup.setAll('body.collideWorldBounds', true);
+    this.playersGroup.setAll('body.immovable', true);
+  },
+
+  updatePlayer: function(player, upKey, downKey) {
+    if(upKey.isDown) {
+      player.body.velocity.y = -properties.player.velocity;
+    }
+    else if(downKey.isDown) {
+      player.body.velocity.y = properties.player.velocity;
+    } else {
+      player.body.velocity.y = 0;
+    }
   },
 
   startGame: function () {
