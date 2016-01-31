062c20b85bd932df55e60c9edfc3a413f9ccfdab
Daniel Garcia <pel_daniel@hotmail.com>
2016-01-30 21:47:06 -0600

    Show player & ball sprites

--- js/game.js
+++ js/game.js
@@ -1,22 +1,70 @@
 var properties = {
-    screenWidth: 800,
-    screenHeight: 400,
+  screenWidth: 800,
+  screenHeight: 400,
+  leftPlayer: {
+    x: 50,
+  },
+  rightPlayer: {
+    x: 750
+  },
 };
 
-var mainState = function(game) { };
+var graphicAssets = {
+  ball: {
+    name: 'ball',
+    url: 'assets/ball.png'
+  },
+  player: {
+    name: 'player',
+    url: 'assets/player.png'
+  },
+};
+
+var mainState = function(game) {
+  this.ballSprite;
+  this.leftPlayerSprite;
+  this.rightPlayerSprite;
+};
 
 mainState.prototype = {
-    preload: function () {
+  preload: function () {
+    game.load.image(graphicAssets.ball.name, graphicAssets.ball.url);
+    game.load.image(graphicAssets.player.name, graphicAssets.player.url);
+  },
 
-    },
+  create: function () {
+    this.initGraphics();
+  },
 
-    create: function () {
+  update: function () {
 
-    },
+  },
 
-    update: function () {
+  initGraphics: function () {
+    // this.backgroundGraphics = game.add.graphics(0, 0);
+    // this.backgroundGraphics.lineStyle(2, 0xFFFFFF, 1);
 
-    },
+    this.ballSprite = game.add.sprite(
+      game.world.centerX,
+      game.world.centerY,
+      graphicAssets.ball.name
+    );
+    this.ballSprite.anchor.set(0.5, 0.5);
+
+    this.leftPlayerSprite = game.add.sprite(
+      properties.leftPlayer.x,
+      game.world.centerY,
+      graphicAssets.player.name
+    );
+    this.leftPlayerSprite.anchor.set(0.5, 0.5);
+
+    this.rightPlayerSprite = game.add.sprite(
+      properties.rightPlayer.x,
+      game.world.centerY,
+      graphicAssets.player.name
+    );
+    this.rightPlayerSprite.anchor.set(0.5, 0.5);
+  },
 };
 
 var game = new Phaser.Game(
